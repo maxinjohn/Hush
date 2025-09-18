0c9fa5e4b8b99fdf4dda1a2bfb13461340240efe
koiisan <107134739+koiverse@users.noreply.github.com>
2025-09-19 06:49:07 +0700

    fix Unresolved reference 'resolved'

--- kizzy/src/main/kotlin/com/my/kizzy/rpc/RpcImage.kt
+++ kizzy/src/main/kotlin/com/my/kizzy/rpc/RpcImage.kt
@@ -28,7 +28,7 @@ sealed class RpcImage {
 
     class ExternalImage(val image: String) : RpcImage() {
         override suspend fun resolveImage(repository: KizzyRepository): String? {
-            return repository.getImage(image)
+            val resolved = repository.getImage(image)
             return if (!resolved.isNullOrBlank()) resolved else image
         }
     }
