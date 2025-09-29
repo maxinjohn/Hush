7e2ba03dec1779e6ef27ab62d2626fe51bf69c5d
koimoee <itzmerinz.dc@gmail.com>
2025-09-30 06:19:22 +0700

    style(Library.kt): add padding imports

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/component/Library.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/component/Library.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -13,6 +14,7 @@ import moe.koiverse.archivetune.utils.rememberPreference
 import moe.koiverse.archivetune.constants.UseNewLibraryDesignKey
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import moe.koiverse.archivetune.innertube.models.PlaylistItem
 import moe.koiverse.archivetune.innertube.models.WatchEndpoint
