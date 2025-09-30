2f15773a594a20985e125de840bcd6d8b35fc8db
koimoee <itzmerinz.dc@gmail.com>
2025-09-30 14:01:42 +0700

    style(LyricsMenu.kt): add heightin layout modifier

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/menu/LyricsMenu.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/menu/LyricsMenu.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
