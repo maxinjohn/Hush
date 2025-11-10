fac7033ec1ff0035935ab90bfd95aef9eb8537c1
Jehan Wijesooriya <windowstechtips3@Gmail.com>
2025-11-10 22:25:20 +0530

    fixes

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/menu/AddToPlaylistDialog.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/menu/AddToPlaylistDialog.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import moe.koiverse.archivetune.innertube.utils.parseCookieString
 import moe.koiverse.archivetune.LocalDatabase
 import moe.koiverse.archivetune.R
