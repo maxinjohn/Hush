0669004d0ae18029b2334180a7b2f454334849a2
koimoee <itzmerinz.dc@gmail.com>
2025-11-15 00:08:16 +0700

    refactor(IntegrationScreen): restructure layout to improve readability and maintainability Lag issues Fixes #46

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/screens/settings/IntegrationScreen.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/screens/settings/IntegrationScreen.kt
@@ -16,6 +16,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.windowInsetsPadding
+import moe.koiverse.archivetune.LocalPlayerAwareWindowInsets
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -54,109 +58,103 @@ fun IntegrationScreen(
 
     var showListenBrainzTokenEditor = remember { mutableStateOf(false) }
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(stringResource(R.string.integration)) },
-                navigationIcon = {
-                    IconButton(
-                        onClick = navController::navigateUp,
-                        onLongClick = navController::backToMain
-                    ) {
-                        Icon(
-                            painterResource(R.drawable.arrow_back),
-                            contentDescription = null
-                        )
-                    }
-                },
-                scrollBehavior = scrollBehavior
+    Column(
+        Modifier
+            .windowInsetsPadding(LocalPlayerAwareWindowInsets.current.only(WindowInsetsSides.Horizontal + WindowInsetsSides.Bottom))
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(
+            Modifier.windowInsetsPadding(
+                LocalPlayerAwareWindowInsets.current.only(
+                    WindowInsetsSides.Top
+                )
             )
-        },
-        content = { innerPadding ->
-            Column(
+        )
+
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surfaceContainer)
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+
+            PreferenceGroupTitle(
+                title = stringResource(R.string.general),
+            )
+
+            Spacer(Modifier.height(12.dp))
+
+            PreferenceEntry(
+                title = { Text(stringResource(R.string.discord_integration)) },
+                icon = { Icon(painterResource(R.drawable.discord), null) },
+                onClick = {
+                    navController.navigate("settings/discord")
+                },
                 modifier = Modifier
-                    .padding(innerPadding)
-                    .background(MaterialTheme.colorScheme.surfaceContainer)
-                    .padding(horizontal = 16.dp, vertical = 12.dp)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(start = 8.dp, end = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.integration),
-                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-                        modifier = Modifier.padding(start = 4.dp)
-                    )
-                    Spacer(modifier = Modifier.weight(1f))
-                }
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.surface)
+            )
 
-                PreferenceGroupTitle(
-                    title = stringResource(R.string.general),
-                )
+            Spacer(Modifier.height(4.dp))
 
-                Spacer(Modifier.height(12.dp))
+            SwitchPreference(
+                title = { Text(stringResource(R.string.listenbrainz_scrobbling)) },
+                description = stringResource(R.string.listenbrainz_scrobbling_description),
+                icon = { Icon(painterResource(R.drawable.token), null) },
+                checked = listenBrainzEnabled,
+                onCheckedChange = onListenBrainzEnabledChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.surface)
+            )
 
-                PreferenceEntry(
-                    title = { Text(stringResource(R.string.discord_integration)) },
-                    icon = { Icon(painterResource(R.drawable.discord), null) },
-                    onClick = {
-                        navController.navigate("settings/discord")
+            Spacer(Modifier.height(4.dp))
+
+            PreferenceEntry(
+                title = { Text(if (listenBrainzToken.isBlank()) stringResource(R.string.set_listenbrainz_token) else stringResource(R.string.edit_listenbrainz_token)) },
+                icon = { Icon(painterResource(R.drawable.token), null) },
+                onClick = { showListenBrainzTokenEditor.value = true },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(MaterialTheme.colorScheme.surface)
+            )
+
+            if (showListenBrainzTokenEditor.value) {
+                TextFieldDialog(
+                    initialTextFieldValue = androidx.compose.ui.text.input.TextFieldValue(listenBrainzToken),
+                    onDone = { data ->
+                        onListenBrainzTokenChange(data)
+                        showListenBrainzTokenEditor.value = false
                     },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(16.dp))
-                        .background(MaterialTheme.colorScheme.surface)
+                    onDismiss = { showListenBrainzTokenEditor.value = false },
+                    singleLine = true,
+                    maxLines = 1,
+                    isInputValid = {
+                        it.isNotEmpty()
+                    },
+                    extraContent = {
+                        InfoLabel(text = stringResource(R.string.listenbrainz_scrobbling_description))
+                    }
                 )
-
-                Spacer(Modifier.height(4.dp))
-
-                SwitchPreference(
-                    title = { Text(stringResource(R.string.listenbrainz_scrobbling)) },
-                    description = stringResource(R.string.listenbrainz_scrobbling_description),
-                    icon = { Icon(painterResource(R.drawable.token), null) },
-                    checked = listenBrainzEnabled,
-                    onCheckedChange = onListenBrainzEnabledChange,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(16.dp))
-                        .background(MaterialTheme.colorScheme.surface)
-                )
-
-                Spacer(Modifier.height(4.dp))
-
-                PreferenceEntry(
-                    title = { Text(if (listenBrainzToken.isBlank()) stringResource(R.string.set_listenbrainz_token) else stringResource(R.string.edit_listenbrainz_token)) },
-                    icon = { Icon(painterResource(R.drawable.token), null) },
-                    onClick = { showListenBrainzTokenEditor.value = true },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(16.dp))
-                        .background(MaterialTheme.colorScheme.surface)
-                )
-
-                if (showListenBrainzTokenEditor.value) {
-                    TextFieldDialog(
-                        initialTextFieldValue = androidx.compose.ui.text.input.TextFieldValue(listenBrainzToken),
-                        onDone = { data ->
-                            onListenBrainzTokenChange(data)
-                            showListenBrainzTokenEditor.value = false
-                        },
-                        onDismiss = { showListenBrainzTokenEditor.value = false },
-                        singleLine = true,
-                        maxLines = 1,
-                        isInputValid = {
-                            it.isNotEmpty()
-                        },
-                        extraContent = {
-                            InfoLabel(text = stringResource(R.string.listenbrainz_scrobbling_description))
-                        }
-                    )
-                }
             }
         }
-    )
+    
+    TopAppBar(
+        title = { Text(stringResource(R.string.integration)) },
+        navigationIcon = {
+            IconButton(
+                onClick = navController::navigateUp,
+                onLongClick = navController::backToMain
+            ) {
+                Icon(
+                    painterResource(R.drawable.arrow_back),
+                    contentDescription = null
+                )
+            }
+        }
+      )
+    }
 }
