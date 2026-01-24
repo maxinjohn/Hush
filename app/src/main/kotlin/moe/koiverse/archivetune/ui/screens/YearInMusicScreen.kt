06c6c6327ef536cb8118f8357b6dbab7c635db05
koiverse <itzmerinz.dc@gmail.com>
2026-01-24 23:26:38 +0700

    style(YearInMusicScreen.kt): add textAlign import

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/screens/YearInMusicScreen.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/screens/YearInMusicScreen.kt
@@ -76,6 +76,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
