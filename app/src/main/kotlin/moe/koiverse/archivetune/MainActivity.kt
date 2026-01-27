9bd8f6f8ff9303359fddc9230d652ade917e6a07
Engine <engine@cto.new>
2026-01-27 16:56:55 +0000

    Update dynamic theme logic for Android 12+ and Android 11-

--- app/src/main/kotlin/moe/koiverse/archivetune/MainActivity.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/MainActivity.kt
@@ -606,7 +606,11 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     } else {
-                        themeColor = DefaultThemeColor
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                            themeColor = DefaultThemeColor
+                        } else {
+                            themeColor = customThemeColor
+                        }
                     }
                 }
             }
