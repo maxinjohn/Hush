c3656705da7f2bdcbaee58bdfb93bac055e31ec3
koiverse <107134739+koiverse@users.noreply.github.com>
2026-02-07 14:41:49 +0000

    Kotlin Header

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/theme/PlayerSliderColors.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/theme/PlayerSliderColors.kt
@@ -1,8 +1,9 @@
 /*
  * ArchiveTune Project Original (2026)
- * Kòi Natsuko
+ * Kòi Natsuko (github.com/koiverse)
  * Licensed Under GPL-3.0 | see git history for contributors
  */
+
 
 package moe.koiverse.archivetune.ui.theme
 
