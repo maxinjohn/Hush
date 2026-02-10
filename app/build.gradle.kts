5864495f33788fc17712f01d148554b19a919976
koiverse <itzmerinz.dc@gmail.com>
2026-02-11 03:46:23 +0700

    build(build): add ktor.client.okhttp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -211,6 +211,7 @@ dependencies {
     implementation("com.github.Kyant0:m3color:2025.4")
 
     implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.websockets)
     implementation(libs.ktor.server.core)
