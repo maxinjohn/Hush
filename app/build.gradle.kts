83489b8c5bb5385dd5304e86e13826212977827f
koiverse <itzmerinz.dc@gmail.com>
2026-03-18 17:00:34 +0700

    bump app version to 13.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     applicationId = "moe.koiverse.archivetune"
         minSdk = 26
         targetSdk = 36
-        versionCode = 130
-        versionName = "12.4.7"
+        versionCode = 131
+        versionName = "13.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
