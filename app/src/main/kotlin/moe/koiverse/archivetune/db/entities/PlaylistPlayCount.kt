a56100a34ab00d0dce80c360f001b81757d6254b
koiverse <itzmerinz.dc@gmail.com>
2026-03-20 10:38:10 +0700

    feat(PlaylistPlayCount.kt): Add PlaylistPlayCount data class

---
+++ app/src/main/kotlin/moe/koiverse/archivetune/db/entities/PlaylistPlayCount.kt
@@ -0,0 +1,14 @@
+/*
+ * ArchiveTune Project Original (2026)
+ * KÃ²i Natsuko (github.com/koiverse)
+ * Licensed Under GPL-3.0 | see git history for contributors
+ */
+
+
+
+package moe.koiverse.archivetune.db.entities
+
+data class PlaylistPlayCount(
+    val playlistId: String,
+    val playCount: Long,
+)
