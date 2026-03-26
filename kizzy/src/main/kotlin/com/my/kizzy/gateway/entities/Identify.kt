d8ecf78aa6e42da0027a2867f85b936a3d632e13
koiverse <itzmerinz.dc@gmail.com>
2026-03-26 15:33:23 +0700

    feat(Identify): update Identify data class with presence and client state fields #401

--- kizzy/src/main/kotlin/com/my/kizzy/gateway/entities/Identify.kt
+++ kizzy/src/main/kotlin/com/my/kizzy/gateway/entities/Identify.kt
@@ -8,6 +8,7 @@
 
 package com.my.kizzy.gateway.entities
 
+import com.my.kizzy.gateway.entities.presence.Presence
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -17,22 +18,36 @@ data class Identify(
     val capabilities: Int,
     @SerialName("compress")
     val compress: Boolean,
-    @SerialName("largeThreshold")
+    @SerialName("large_threshold")
     val largeThreshold: Int,
     @SerialName("properties")
     val properties: Properties,
+    @SerialName("presence")
+    val presence: Presence? = null,
+    @SerialName("client_state")
+    val clientState: ClientState? = null,
     @SerialName("token")
     val token: String,
 ) {
     companion object {
         fun String.toIdentifyPayload() = Identify(
-            capabilities = 65,
+            capabilities = 16381,
             compress = false,
-            largeThreshold = 100,
+            largeThreshold = 250,
             properties = Properties(
-                browser = "Discord Client",
-                device = "ktor",
-                os = "Windows"
+                browser = "disco",
+                device = "disco",
+                os = "Android"
+            ),
+            presence = Presence(
+                activities = emptyList(),
+                afk = false,
+                since = 0,
+                status = "unknown",
+            ),
+            clientState = ClientState(
+                apiCodeVersion = 0,
+                guildVersions = emptyMap(),
             ),
             token = this
         )
@@ -47,4 +62,12 @@ data class Properties(
     val device: String,
     @SerialName("os")
     val os: String,
+)
+
+@Serializable
+data class ClientState(
+    @SerialName("api_code_version")
+    val apiCodeVersion: Int = 0,
+    @SerialName("guild_versions")
+    val guildVersions: Map<String, Int> = emptyMap(),
 )
