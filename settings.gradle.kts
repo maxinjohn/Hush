056a91f77192dc9b64434a0b30e84368c2d75aff
koiverse <itzmerinz.dc@gmail.com>
2026-04-10 00:30:38 +0700

    build(settings): Add Sonatype Maven Snapshots repository

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { setUrl("https://central.sonatype.com/repository/maven-snapshots/") }
         maven { setUrl("https://jitpack.io") }
     }
 }
