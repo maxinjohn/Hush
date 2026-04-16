356bdddb2fa7b4e1a2d132818513474d802eef28
koiverse <itzmerinz.dc@gmail.com>
2026-04-16 23:44:12 +0700

    refactor(RemoteHistorySyncManager): remove unused androidx.datastore.preferences.core.remove import

--- app/src/main/kotlin/moe/koiverse/archivetune/utils/RemoteHistorySyncManager.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/utils/RemoteHistorySyncManager.kt
@@ -13,7 +13,6 @@ package moe.koiverse.archivetune.utils
 import android.content.Context
 import androidx.compose.runtime.Immutable
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.remove
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
