4289508a9b176a964461f3ac1f13dba3acccce6f
koiverse <itzmerinz.dc@gmail.com>
2026-04-25 01:34:14 +0700

    fix(AlbumMenu): add missing Column import

--- app/src/main/kotlin/moe/koiverse/archivetune/ui/menu/AlbumMenu.kt
+++ app/src/main/kotlin/moe/koiverse/archivetune/ui/menu/AlbumMenu.kt
@@ -19,6 +19,7 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
