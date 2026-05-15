78d6455adc9e654b92b2fb88eb542a15a4c0f4ac
koiverse <107134739+koiverse@users.noreply.github.com>
2026-05-15 05:04:31 +0000

    Kotlin Header

--- shazamkit/src/main/kotlin/moe/koiverse/archivetune/shazamkit/models/ShazamModels.kt
+++ shazamkit/src/main/kotlin/moe/koiverse/archivetune/shazamkit/models/ShazamModels.kt
@@ -1,9 +1,10 @@
 /*
- * ArchiveTune Project Original (2026)
- * Chartreux Westia (github.com/koiverse)
- * Licensed Under GPL-3.0 | see git history for contributors
- * Don't remove this copyright holder!
+ * ArchiveTune (2026)
+ * © Chartreux Westia — github.com/koiverse
+ * GPL-3.0 License | Contributors: see git history
+ * Do not remove or alter this notice. - Per GPL-3.0 Section 4 & Section 5
  */
+
 
 package moe.koiverse.archivetune.shazamkit.models
 
